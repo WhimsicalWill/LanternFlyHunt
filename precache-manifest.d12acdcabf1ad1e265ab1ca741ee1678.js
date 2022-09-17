b19d41932966b7703d29e769c987ff27d49a2849
whimsicalwill <knipewilliam20@gmail.com>
2022-09-17 17:49:13 -0400

    Updates

---
+++ precache-manifest.d12acdcabf1ad1e265ab1ca741ee1678.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95a7c2f6e16996a6b57e5f757bbe418e",
+    "url": "/LanternFlyHunt/index.html"
+  },
+  {
+    "revision": "73dbac58ca9dea486b12",
+    "url": "/LanternFlyHunt/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "f02a8d49f84d2f96064f",
+    "url": "/LanternFlyHunt/static/js/2.2a85daf2.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/LanternFlyHunt/static/js/2.2a85daf2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73dbac58ca9dea486b12",
+    "url": "/LanternFlyHunt/static/js/main.9e23689c.chunk.js"
+  },
+  {
+    "revision": "4cb84708fe844cfea101",
+    "url": "/LanternFlyHunt/static/js/runtime-main.40fbd24c.js"
+  }
+]);
